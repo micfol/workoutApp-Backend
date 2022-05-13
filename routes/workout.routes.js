aabd9c625568a1d1b5c96890850a9bf61a2afe47
George DS Brooks <george.ds.brooks@gmail.com>
2022-05-13 19:31:51 -0400

    user model includes working weight and workout route updates them

--- routes/workout.routes.js
+++ routes/workout.routes.js
@@ -23,7 +23,7 @@ router.post("/exercisetype", async (req, res) => {
 router.delete("/workout/:workoutId", async (req, res) => {
     try {
         await Workout.findByIdAndDelete(req.params.workoutId);
-        res.status(200).json({ message: `Workout with ID ${req.params.workoutId} was deleted.`});
+        res.status(200).json({ message: `Workout with ID ${req.params.workoutId} was deleted.` });
     }
     catch {
         res.status(500).json({ message: e });
@@ -66,25 +66,39 @@ router.get("/workout", async (req, res) => {
 router.post("/exerciseentry", async (req, res) => {
     try {
         const { isWorkoutA, workoutExercises, user } = req.body;
-        workoutExercises.forEach((x) => { 
-            x.user = user         // Adds _id to each exercise object
-        });
-
-        const responseExercise = await Exercise.create(workoutExercises, {new: true}); // Creates 3 new exercies entries
-        const exerciseIDs = responseExercise.map((x) => x._id); //returns array of the 3 created entry _ids.\
+       const exercises = workoutExercises.map((exercise) => {
+            return {...exercise[1], user: user} 
+        })
+        const weightIncrease = {
+            squat: 2.5,
+            bench: 2.5,
+            row: 2.5,
+            military: 2.5,
+            deadlift: 5
+        }
+        console.log('workoutExercises', workoutExercises)
+        const workingWeight = workoutExercises.map((exercise) => {
+            if (exercise[1].sets.every(set => set === 5)) {
+                exercise[1].weight = exercise[1].weight + weightIncrease[exercise[0]]
+            }
+            return [exercise[0], exercise[1].weight]
+        })
+        console.log('workingWeight', workingWeight)
+        const workingWeightSquats = Object.fromEntries(workingWeight)
+        console.log('workingWeightSquats', workingWeightSquats)
         
+        const responseExercise = await Exercise.create(exercises, { new: true }); // Creates 3 new exercies entries
+        const exerciseIDs = responseExercise.map((x) => x._id); //returns array of the 3 created entry _ids.\
+
         // const totalWeightLifted = workoutExercises.map(exercise => exercise.sets)
 
         // console.log('totalWeightLifted', totalWeightLifted)
-        const totalWeightLifted = workoutExercises.map(exercise => exercise.sets)
-        console.log('totalWeightLifted', totalWeightLifted)
         console.log('isWorkoutA :>> ', isWorkoutA);
-        const responseWorkout = await Workout.create({isWorkoutA, exercises: exerciseIDs, totalWeightLifted: 0, user});
-        
-        const responseUpdateUser = await User.findByIdAndUpdate(user, { $addToSet: { exerciseEntries: exerciseIDs, workoutEntries: responseWorkout._id } }, {new: true} ).populate('workoutEntries')
-        
-        console.log('3 responses', responseExercise, responseWorkout, responseUpdateUser)
-        res.status(200).json({responseUpdateUser})
+        const responseWorkout = await Workout.create({ isWorkoutA, exercises: exerciseIDs, work: 0, user });
+
+        const responseUpdateUser = await User.findByIdAndUpdate(user, { $addToSet: { exerciseEntries: exerciseIDs, workoutEntries: responseWorkout._id, }, ...workingWeightSquats }, { new: true }).populate('workoutEntries')
+
+        res.status(200).json({ responseUpdateUser })
     }
     catch (e) {
         console.log('e', e)
@@ -94,11 +108,10 @@ router.post("/exerciseentry", async (req, res) => {
 
 // GET Route to Populate Total Workout Session Information by User 
 router.get("/progress/:id", async (req, res) => {
-    try{
+    try {
         const id = req.params.id;
-        const userWorkoutSessions = await Workout.find({ user: id }).populate({path: 'exercises'});
-        const sortedbyNewest = [...userWorkoutSessions].sort((a,b) => Date.parse(b.createdAt) - Date.parse(a.createdAt))
-        console.log('sortbyNewest', sortedbyNewest)
+        const userWorkoutSessions = await Workout.find({ user: id }).populate({ path: 'exercises' });
+        const sortedbyNewest = [...userWorkoutSessions].sort((a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt))
         res.status(200).json(sortedbyNewest);
     }
     catch (e) {
