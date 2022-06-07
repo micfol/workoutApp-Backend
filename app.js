8a55f4c34354bb0b2a5e0f7ba12b1d061505d079
Michael Foley <foley.p.michael@gmail.com>
2022-06-07 12:17:14 +0100

    moved public/index route back to end and commented out index routes

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 require("./config")(app);
 
-const allRoutes = require("./routes/index.routes");
-app.use("/api", allRoutes);
+// const allRoutes = require("./routes/index.routes");
+// app.use("/api", allRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/api", authRoutes);
@@ -20,12 +20,12 @@ app.use("/api", workoutRoutes);
 const educationRoutes = require("./routes/education.routes");
 app.use("/api", educationRoutes);
 
-app.use((req, res, next) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
+
   
 require("./error-handling")(app);
 
-
+app.use((req, res, next) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
 module.exports = app;
